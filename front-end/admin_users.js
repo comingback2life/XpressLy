732912baf9f9c5eebb590d063fbada921f491ab0
Gorock Shetty <gorock397@gmail.com>
2021-08-16 00:47:53 +1000

    Admin UI pages added to front-end

---
+++ front-end/admin_users.js
@@ -0,0 +1,8 @@
+function menuToggle(){
+    let toggle = document.querySelector('.toggle');
+    let sideBar = document.querySelector('.side-bar');
+    let main = document.querySelector('.main');
+    toggle.classList.toggle('active');
+    sideBar.classList.toggle('active');
+    main.classList.toggle('active');
+}
